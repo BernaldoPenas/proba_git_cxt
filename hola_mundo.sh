c26b09a67a6552bb69ff15ff9f136646be0b48b2
abdalazhred@hotmail.com <abdalazhred@Innsmouth>
2014-05-12 22:44:19 +0200

    Creada nova rama

--- hola_mundo.sh
+++ hola_mundo.sh
@@ -1,32 +1,46 @@
 #! /bin/sh
 
-echo "Right! now ha, ha
-I am an anti-christ
-i am an anarchist,
-don't know what i want
-but i know how to get it.
-i wanna destroy the passer by
-'cos i wanna be anarchy,
-no dogs body
-Anarchy for the uk
-it's coming sometime and maybe
-i give a wrong time stop a traffic line.
-your future dream is a shopping scheme
-cause i wanna be anarchy,
-it's in the city
-How many ways to get what you want
-i use the best i use the rest
-i use the enemy.
-i use anarchy
-'cause i wanna be anarchy,
-Its the only way to be
-Is this the mpla
-or is this the uda
-or is this the ira
-i thought it was the uk
-or just another country
-another council tenancy.
-I wanna be an anarchist
-(oh what a name)
-and l wanna be an anarchist
-(i get pissed destroy)"
+echo "God save the queen
+the fascist regime,
+they made you a moron
+a potential h-bomb.
+God save the queen
+she ain't no human being.
+there is no future
+in england's dreaming
+Don't be told what you want
+don't be told what you need.
+there's no future
+there's no future
+there's no future for you
+God save the queen
+we mean it man
+we love our queen
+god saves
+God save the queen
+'cos tourists are money
+and our figurehead
+is not what she seems
+Oh god save history
+god save your mad parade
+oh lord god have mercy
+all crimes are paid.
+When there's no future
+how can there be sin
+we're the flowers
+in the dustbin
+we're the poison
+in your human machine
+we're the future
+you're future
+God save the queen
+we mean it man
+we love our queen
+god saves
+God save the queen
+we mean it man
+there is no future
+in england's dreaming
+No future
+no future for you
+no fufure for me"
