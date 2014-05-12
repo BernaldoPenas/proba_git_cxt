facbae5e4b29e259a6023d2e8f24782b41ef51ff
abdalazhred@hotmail.com <abdalazhred@Innsmouth>
2014-05-12 22:37:45 +0200

    Engadido un novo arquivo, modificado README

---
+++ hola_mundo.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+echo "Right! now ha, ha
+I am an anti-christ
+i am an anarchist,
+don't know what i want
+but i know how to get it.
+i wanna destroy the passer by
+'cos i wanna be anarchy,
+no dogs body
+Anarchy for the uk
+it's coming sometime and maybe
+i give a wrong time stop a traffic line.
+your future dream is a shopping scheme
+cause i wanna be anarchy,
+it's in the city
+How many ways to get what you want
+i use the best i use the rest
+i use the enemy.
+i use anarchy
+'cause i wanna be anarchy,
+Its the only way to be
+Is this the mpla
+or is this the uda
+or is this the ira
+i thought it was the uk
+or just another country
+another council tenancy.
+I wanna be an anarchist
+(oh what a name)
+and l wanna be an anarchist
+(i get pissed destroy)"
